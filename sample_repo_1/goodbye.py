65d4fe9bae271b31fd54968c07150d65c56841eb
ChiroMSHI <millermon244@msn.com>
2024-12-08 17:36:10 -0600

    Add files via upload

---
+++ sample_repo_1/goodbye.py
@@ -0,0 +1,7 @@
+
+def say_goodbye():
+    print("Take care and I hope you have a great day!")
+
+
+if __name__ == '__main__':
+    say_goodbye()
---
+++ sample_repo_1/hello.py
@@ -0,0 +1,9 @@
+
+def howdy():
+    print("Hello and welcome!")
+
+
+if __name__ == '__main__':
+    howdy()
+
+
